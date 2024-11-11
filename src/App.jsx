e583ea48d24deeaefea5920da85e1f5d05579927
Suryakant01 <suryakant160502@gmail.com>
2024-11-11 21:02:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Padhai kro</h1>
+        <iframe width="315" height="560"
+          src="https://www.youtube.com/embed/0gBNeiy6auQ"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen></iframe>
+
+
+      </div>
+    </>
+  )
+}
+
+export default App
